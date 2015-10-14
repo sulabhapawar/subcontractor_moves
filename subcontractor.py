6526b81dc09f2453cc2fbeff3521da52320a3789
sulabha <sulabhapawar7@gmail.com>
2015-10-14 11:03:31 +0530

    delivery order

--- subcontractor.py
+++ subcontractor.py
@@ -4,13 +4,49 @@ class subcontractor_moves(osv.osv):
     _name='subcontractor.moves'
     _columns={
               'partner_id':fields.many2one('res.partner',"Supplier"),
-              'move_lines':fields.one2many('stock.move','subcontractor_picking',"Products")
+            'move_lines':fields.one2many('stock.move','subcontractor_picking',"Products")
               }
-
+    
+    def send_to_subcontractor(self,cr,uid,ids,context=None):
+        sub_obj=self.pool.get('subcontractor.moves').browse(cr,uid,ids,context=context)
+        sup_partner=sub_obj.partner_id.id
+        list_moves=[]
+        for m in sub_obj.move_lines:
+            list_moves.append(m.id)
+        
+            out_vals={
+                      'partner_id':sup_partner,
+                      'picking_type_id':m.picking_type_id.id,
+                      'location_id':m.location_id.id,
+                      'location_dest_id':m.location_dest_id.id,
+                      'group_id':m.group_id.id,
+                      }
+            pick_id=self.pool.get('stock.picking').create(cr,uid,out_vals,context=context)
+                
+            self.pool.get('stock.move').write(cr,uid,list_moves,{
+                                                                 'picking_id':pick_id,
+                                                                 },context=context)
+        
+        
+#         for l in sub_obj.move_lines:
+#             in_vals={
+#                       'partner_id':sup_partner,
+#                       'picking_type_id':1,
+#                       'location_id':l.location_dest_id.id,
+#                       'location_dest_id':l.location_id.id,
+#                       }
+#             pick_id1=self.pool.get('stock.picking').create(cr,uid,in_vals,context=context)
+#                    
+#             self.pool.get('stock.move').write(cr,uid,list_moves,{
+#                                                                  'picking_id':pick_id1,
+#                                                              },context=context)
+        
+        
+        
 class stoke_move(osv.osv):
     _inherit="stock.move"
     _columns={
-              'subcontractor_picking':fields.many2one('subcontractor.moves','Subcontractor Picking')
+            'subcontractor_picking':fields.many2one('subcontractor.moves','Subcontractor Picking')
               } 
     
 class stock_picking(osv.osv):
@@ -30,7 +66,6 @@ class stock_picking(osv.osv):
               'move_lines':False
               }
         sub_id=self.pool.get('subcontractor.moves').create(cr,uid,vals,context=context)
-        
         self.pool.get('stock.move').write(cr,uid,list_moves,{'subcontractor_picking':sub_id},context)
 
         return{
